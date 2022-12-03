3be79fca54235bd5b4e86fad1104a14394b5c9ae
Leandro Fonseca <leandromkt@outlook.com>
2022-12-03 14:19:48 -0300

    Componentes do Design System - componente: Box

---
+++ packages/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,17 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Box, BoxProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Surface/Box',
+  component: Box,
+
+  args: {
+    children: (
+      <>
+        <span>Testando o elemento Box</span>
+      </>
+    ),
+  },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
