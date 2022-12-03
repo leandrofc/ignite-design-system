3546bc87c169704bae76a770cb7722e292be87aa
Leandro Fonseca <leandromkt@outlook.com>
2022-12-03 14:42:20 -0300

    Componentes do Design System - Componente: Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum perspiciatis consequatur praesentium earum id sunt tempora ullam quaerat, esse quisquam, unde deserunt ut numquam debitis sit et quasi asperiores eaque?',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
