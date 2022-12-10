35a380c629101126fe702c6a7b2029dacaac7d34
Leandro Fonseca <leandromkt@outlook.com>
2022-12-10 14:49:59 -0300

    Melhorias no Storybook - Controles nos componentes

--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -5,8 +5,31 @@ export default {
   title: 'Typography/Text',
   component: Text,
   args: {
+    size: 'md',
     children:
       'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum perspiciatis consequatur praesentium earum id sunt tempora ullam quaerat, esse quisquam, unde deserunt ut numquam debitis sit et quasi asperiores eaque?',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
